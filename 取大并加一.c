56d724e3fc5c0a6f14dfb967dbb413214a30454f
JHM2004 <tql_qaq@tju.edu.cn>
2026-06-08 17:09:05 +0800

    update

---
+++ 取大并加一.c
@@ -0,0 +1,1 @@
+#include <iostream> using namespace std;     int Max(int x ,int y) {     If(x>y)       return x;      else         return y; } int main() {     int a=0;     int b=0;     scanf("%d %d",&a ,&b);     int m=Max(a,b);     printf("%d\n",m+1);     return 0; }
